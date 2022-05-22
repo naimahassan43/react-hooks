3abb1244cc32caaf153b2f00d00a021cc5da473b
Naima Hassan <naima.hassan434@gmail.com>
2022-05-22 23:31:23 +0100

    Simple counter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 // import Setup from './tutorial/1-useState/1-error-example'
 // import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
 // import UseStateArray from "./tutorial/1-useState/3-useState-array";
-import UseStateObject from "./tutorial/1-useState/4-useState-object";
+// import UseStateObject from "./tutorial/1-useState/4-useState-object";
+import UseStateCounter from "./tutorial/1-useState/5-useState-counter";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Setup/> */}
       {/* <UseStateBasics/> */}
       {/* <UseStateArray /> */}
-      <UseStateObject />
+      {/* <UseStateObject /> */}
+      <UseStateCounter />
     </div>
   );
 }
