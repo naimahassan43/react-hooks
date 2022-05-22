95eb760a9956219bb477c6739b1dba20dd0fffbc
Naima Hassan <naima.hassan434@gmail.com>
2022-05-22 04:05:14 +0100

    useState-Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/1-error-example'
+// import Setup from './tutorial/1-useState/1-error-example'
+import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
 
 function App() {
   return (
     <div className='container'>
-      <Setup/>
+      {/* <Setup/> */}
+      <UseStateBasics/>
     </div>
   )
 }
--- src/tutorial/1-useState/2-useState-basics.js
+++ src/tutorial/1-useState/2-useState-basics.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+//useState - function
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+
+  const [text, setText] = useState("random title");
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("Hello Title");
+    } else {
+      setText("random title");
+    }
+  };
+
+  
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
