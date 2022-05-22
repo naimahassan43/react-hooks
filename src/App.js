8c023a85f47985615c468233f864832a756c00c7
Naima Hassan <naima.hassan434@gmail.com>
2022-05-22 14:10:37 +0100

    useState - Array Example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 // import Setup from './tutorial/1-useState/1-error-example'
-import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
+// import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
+import UseStateArray from "./tutorial/1-useState/3-useState-array";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       {/* <Setup/> */}
-      <UseStateBasics/>
+      {/* <UseStateBasics/> */}
+      <UseStateArray />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
