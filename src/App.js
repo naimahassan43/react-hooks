57ff22dccebca501793da62ead2cfdfdf2560fb4
Naima Hassan <naima.hassan434@gmail.com>
2022-05-23 13:04:57 +0100

    useEffect - Basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-// import Setup from './tutorial/1-useState/1-error-example'
-// import UseStateBasics from "./tutorial/1-useState/2-useState-basics";
-// import UseStateArray from "./tutorial/1-useState/3-useState-array";
-// import UseStateObject from "./tutorial/1-useState/4-useState-object";
-import UseStateCounter from "./tutorial/1-useState/5-useState-counter";
+import Setup from "./tutorial/2-useEffect/1-useEffect-basics";
+
 
 function App() {
   return (
     <div className="container">
-      {/* <Setup/> */}
-      {/* <UseStateBasics/> */}
-      {/* <UseStateArray /> */}
-      {/* <UseStateObject /> */}
-      <UseStateCounter />
+      <Setup/>
+      
     </div>
   );
 }
