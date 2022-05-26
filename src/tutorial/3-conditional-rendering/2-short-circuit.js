7d404034dc97c526d51c51a28bbc2a14c6133f9d
Naima Hassan <naima.hassan434@gmail.com>
2022-05-26 19:59:22 +0100

    Short-Circuit Evaluation

--- src/tutorial/3-conditional-rendering/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/2-short-circuit.js
@@ -3,10 +3,19 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hi everyone';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h2>{firstValue}</h2>
+      <h2>value : {secondValue}</h2> */}
+      <h1>{text||'John Smith'}</h1>
+      {text && <h1> Hello!!!</h1>}
+      {!text && <h1> Hello everyone!!!</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
