ea883027007b86856be97e7ba9db4731fd1dec01
Naima Hassan <naima.hassan434@gmail.com>
2022-05-29 11:22:05 +0100

    Multiple Inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/4-forms/1-controlled-inputs";
-
+import Setup from "./tutorial/4-forms/2-multiple-inputs";
 
 function App() {
   return (
     <div className="container">
-      <Setup/>
-      
+      <Setup />
     </div>
   );
 }
