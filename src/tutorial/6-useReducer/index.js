1ec4c006e7ff4c8991c1f068cb2d873b8d570167
Naima Hassan <naima.hassan434@gmail.com>
2022-05-31 17:42:04 +0100

    useReducer-Add item

--- src/tutorial/6-useReducer/index.js
+++ src/tutorial/6-useReducer/index.js
@@ -3,12 +3,28 @@ import Modal from "./Modal";
 import { data } from "../../data";
 // reducer function
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "item added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return { ...state, isModalOpen: true, modalContent: "please enter value" };
+  }
+  throw new Error("no matching action type found");
+};
+
 const defaultState = {
   people: [],
   isModalOpen: false,
   modalContent: "",
 };
+
 const Index = () => {
   const [name, setName] = useState("");
   const [state, dispatch] = useReducer(reducer, defaultState);
@@ -16,9 +32,14 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName("");
     } else {
+      dispatch({ type: "NO_VALUE" });
     }
   };
+
   return (
     <>
       {state.isModalOpen && <Modal modalContent={state.modalContent} />}
